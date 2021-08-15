408542c735bffdc4266fd3cf8ce7ff4a5f902cc4
Sanket2055 <sanketdiwate95@gmail.com>
2021-08-15 16:16:04 +0530

    added routing to homepage using router HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
 import './App.css';
+import { Route, Switch } from 'react-router';
 import HomePage from './pages/homepage/homepage.components';
+const HatsPage=()=>{
+  return(
+    <div>
+      <h1>hatspage</h1>
+    </div>
+  )
+}
 function App() {
   return (
     <div >
-      <HomePage/>
+      <Switch>
+      <Route exact path='/' component={HomePage}></Route>
+      <Route  path='/shop/hats' component={HatsPage}></Route>
+      </Switch>
+      
     </div>
   );
 }
--- src/components/menu-items/menu-item.component.jsx
+++ src/components/menu-items/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './menu-item.styles.scss';
-const MenuItem=({title ,imageurl , id ,size})=>{
+const MenuItem=({title ,imageUrl , id ,size ,history , linkUrl, match})=>{
     return(
-        <div className={`${size} menu-item`} >
-            <div className="background-image"  style={{backgroundImage:`url(${imageurl})`}}/>
+        <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+            <div className="background-image"  style={{backgroundImage:`url(${imageUrl})`}}/>
                 <div className="content">
                     <h1 className="title">{title.toUpperCase()}</h1>
                     <span className="subtitle">SHOP NOW!</span>
@@ -11,4 +12,4 @@ const MenuItem=({title ,imageurl , id ,size})=>{
             </div>
     );
 }
-export default MenuItem;
+export default withRouter(MenuItem);
