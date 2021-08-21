e6e30937f8ed377a1c433d44a4d40cc693120edc
Sanket2055 <sanketdiwate95@gmail.com>
2021-08-21 16:26:33 +0530

    added sign in authentication with firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,6 +10,28 @@ const config = {
     appId: "1:771805416191:web:ac54e17f0462836c84daca",
     measurementId: "G-BESRPHDCE4"
   };
+  export const createUserProfileDocument = async (userAuth , additionalData)=>{
+    if(!userAuth)return;
+    const userRef=firestore.doc(`users/${userAuth.uid}`);
+    const snapShot=await userRef.get();
+    if(!snapShot.exists){
+      const {displayName ,email}=userAuth;
+      const createdAt=new Date();
+      try{
+        await userRef.set({
+          displayName,
+          email,
+          createdAt,
+          ...additionalData
+        })
+      }catch(error){
+        console.log("error creating user" , error.message);
+
+      }
+    }
+    return userRef;
+    // console.log(snapShot);
+  }
   firebase.initializeApp(config);
   export const auth =firebase.auth();
   export const firestore =firebase.firestore();
